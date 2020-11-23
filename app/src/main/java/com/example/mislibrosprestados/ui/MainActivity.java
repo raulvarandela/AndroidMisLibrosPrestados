c96e02ab0ae406beb2a505249656679c90243b1d
Raul Varandela <raulvarandela@protonmail.com>
2020-11-23 10:54:17 +0100

    Añadido el cursor
    Signed-off-by: Raul Varandela <raulvarandela@protonmail.com>

--- app/src/main/java/com/example/mislibrosprestados/ui/MainActivity.java
+++ app/src/main/java/com/example/mislibrosprestados/ui/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         //inicializar vistas
         final ListView LV_LIBROS = this.findViewById(R.id.lvLibros);
         final Button BT_INSERTA = this.findViewById(R.id.btInserta);
+        final Button BT_VER = this.findViewById(R.id.btVer);
 
         BT_INSERTA.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,11 +39,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        BT_VER.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ver();
+            }
+        });
+
+
         this.cursorAdapter = new SimpleCursorAdapter(this,
                 R.layout.entrada_libro_layout,
                 null,
                 new String[]{SqlIO.CAMPO_LIBRO_TITULO, SqlIO.CAMPO_LIBRO_DIAS_PRESTAMO},
-                new int[]{R.id.lblTitulo,R.id.lblDias},
+                new int[]{R.id.lblTitulo, R.id.lblDias},
                 0);
 
         LV_LIBROS.setAdapter(cursorAdapter);
@@ -64,8 +75,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void actualiza(){
+    public void actualiza() {
         this.cursorAdapter.swapCursor(this.sqlIO.getCurosTodosLibros());
+    }
+
+    private void ver() {
+        final AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+        final StringBuilder toret = new StringBuilder();
+        final Cursor CURSOR_LIBROS = this.sqlIO.getCurosTodosLibros();
+        final int index = CURSOR_LIBROS.getColumnIndexOrThrow(sqlIO.CAMPO_LIBRO_TITULO);
+
+        CURSOR_LIBROS.getCount();
+
+        if (CURSOR_LIBROS.moveToFirst()) {
+            do {
+                toret.append(CURSOR_LIBROS.getString(index));
+                toret.append("\n");
+            } while (CURSOR_LIBROS.moveToNext());
+        }
+
+        dlg.setTitle("Libros");
+        dlg.setMessage(toret.toString());
+        dlg.create().show();
+
     }
 
     private SimpleCursorAdapter cursorAdapter;
